2227e64dcd72a64f25b20bf7204461cc4f6ac046
kevinalbert7 <20ko@live.fr>
2021-11-17 19:05:06 +0100

    affichagePokemon

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react'
+import { useState, useEffect } from 'react'
 
 const Home = () => {
+    // console.log(useState)
+    const [pokemon, setPokemon] = useState(null)
+    console.log(pokemon)
+    console.log(setPokemon)
+
+    useEffect(() => {
+        fetch(`https://pokeapi.co/api/v2/pokemon/1`)
+        .then(response => response.json())
+        .then(data => setPokemon(data) )
+    }, [])
+    
+    
+    console.log(pokemon)
+
+    if ( !pokemon ){
+        return (null)
+    }
     return (
         <div>
             <p>HOME</p>
+           <p> {pokemon.name}</p>
+
+     
         </div>
-    );
+    )
 }
 
 export default Home
