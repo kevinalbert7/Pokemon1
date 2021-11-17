d9a92de6b46e5da4ce626be0bd7b47b61dbca7f7
kevinalbert7 <20ko@live.fr>
2021-11-17 15:55:13 +0100

    PokeCreation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home}/> 
+        <Route path="/login" component={Login}/>
+        <Route path="*" component={Error404}/> 
+      </Switch>
     </div>
+  </BrowserRouter>
   );
 }
 
